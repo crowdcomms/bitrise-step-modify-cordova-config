a5fd9ac78768c4ddb2874a55a24c888c43606557
Daniel Douglas <dan.douglas.01@gmail.com>
2020-04-16 22:00:41 +0100

    task(step): pass test value to index.js

--- index.js
+++ index.js
@@ -15,32 +15,38 @@ commander
     .parse(process.argv);
 
 (() => {
-    // const components = commander.bundleId.split('.');
-    // const urlName = components[components.length - 1];
-    // writeConfigXml(urlName);
+    const components = commander.bundleId.split('.');
+    const urlName = components[components.length - 1];
+    writeConfigXml(urlName);
 
     // writePackageJson(urlName);
 
     // createResources();
 
-    const config = new Config(commander.pathToConfig);
+    // const config = new Config(commander.pathToConfig);
 
-    console.log('Name' + commander.bundleName);
-    if (commander.bundleName) {
-      config.setName(`${commander.bundleName}`);
-    }
+    // console.log('COnfig: %o', Config);
+
+    // console.log('Name ' + commander.bundleName);
+    // console.log('bundleId ' + commander.bundleId);
+    // console.log('iosVersion ' + commander.iosVersion);
+    // console.log('androidVersion ' + commander.androidVersion);
+    // console.log('iosBuildNumber ' + commander.iosBuildNumber);
+    // if (commander.bundleName) {
+    //   config.setName(`${commander.bundleName}`);
+    // }
     
-    if (commander.bundleId) {
-      config.setID(`${commander.bundleId}`);
-    }
+    // if (commander.bundleId) {
+    //   config.setID(`${commander.bundleId}`);
+    // }
     
-    if (commander.iosVersion) {
-      config.setVersion(`${commander.iosVersion}`);
-    }
+    // if (commander.iosVersion) {
+    //   config.setVersion(`${commander.iosVersion}`);
+    // }
     
-    if (commander.androidVersion) {
-      config.setAndroidVersionCode(`${commander.androidVersion}`);
-    }
+    // if (commander.androidVersion) {
+    //   config.setAndroidVersionCode(`${commander.androidVersion}`);
+    // }
     
     // if (ANDROID_PACKAGE_NAME) {
     //   config.setAndroidPackageName(ANDROID_PACKAGE_NAME);
@@ -48,9 +54,9 @@ commander
     //   log('android-packageName', ANDROID_PACKAGE_NAME);
     // }
     
-    if (commander.iosBuildNumber) {
-      config.setIOSBundleVersion(`${commander.iosBuildNumber}`);
-    }
+    // if (commander.iosBuildNumber) {
+    //   config.setIOSBundleVersion(`${commander.iosBuildNumber}`);
+    // }
     
     // if (IOS_BUNDLE_IDENTIFIER) {
     //   config.setIOSBundleIdentifier(IOS_BUNDLE_IDENTIFIER);
@@ -59,61 +65,61 @@ commander
     // }
     
     // Write the config file
-    config.writeSync();
+    // config.writeSync();
     
 
 
 })();
 
-// function writeConfigXml(urlName) {
-//     try {
-//         console.log('Path: ' + commander.pathToConfig);
-//         const xml = fs.readFileSync(commander.pathToConfig, 'utf8');
+function writeConfigXml(urlName) {
+    try {
+        console.log('Path: ' + commander.pathToConfig);
+        const xml = fs.readFileSync(commander.pathToConfig, 'utf8');
 
-//         xml2js.parseString(xml, (err, obj) => {
-//           console.log(commander.bundleId)
-//             if ('Bundle id: ' + commander.bundleId) {
-//               console.log(obj.widget.$.id);
-//                 obj.widget.$.id = commander.bundleId;
-//             }
+        xml2js.parseString(xml, (err, obj) => {
+          console.log(commander.bundleId)
+            if (commander.bundleId) {
+              console.log(obj.widget.$.id);
+                obj.widget.$.id = commander.bundleId;
+            }
 
-//             if (commander.bundleName) {
-//                 obj.widget.name[0] = commander.bundleName
-//             }
+            if (commander.bundleName) {
+                obj.widget.name[0] = commander.bundleName
+            }
 
-//             if (commander.androidVersion) {
-//                 obj.widget.$['android-versionCode'] = commander.androidVersion;
-//             }
+            if (commander.androidVersion) {
+                obj.widget.$['android-versionCode'] = commander.androidVersion;
+            }
 
-//             if (commander.iosVersion) {
-//                 obj.widget.$['version'] = commander.iosVersion;
-//             }
+            if (commander.iosVersion) {
+                obj.widget.$['version'] = commander.iosVersion;
+            }
 
-//             if (commander.iosBuildNumber) {
-//                 obj.widget.$['ios-CFBundleVersion'] = commander.iosBuildNumber;
-//             }
+            if (commander.iosBuildNumber) {
+                obj.widget.$['ios-CFBundleVersion'] = commander.iosBuildNumber;
+            }
 
-//             // Update values for deeplinks
-//             const plugin = obj.widget.plugin.filter(elem => elem.$.name == 'ionic-plugin-deeplinks' && !!elem.variable)[0];
-//             if (!!plugin) {
-//                 let variable = plugin.variable.find(elem => elem.$.name == 'URL_SCHEME');
-//                 if (!!variable) {
-//                     variable.$['value'] = urlName;
-//                 }
-//             }
+            // Update values for deeplinks
+            const plugin = obj.widget.plugin.filter(elem => elem.$.name == 'ionic-plugin-deeplinks' && !!elem.variable)[0];
+            if (!!plugin) {
+                let variable = plugin.variable.find(elem => elem.$.name == 'URL_SCHEME');
+                if (!!variable) {
+                    variable.$['value'] = urlName;
+                }
+            }
 
-//             var builder = new xml2js.Builder();
-//             var xml = builder.buildObject(obj);
+            var builder = new xml2js.Builder();
+            var xml = builder.buildObject(obj);
 
-//             console.log(`Updating config.xml file...`);
-//             fs.writeFile(commander.pathToConfig, xml, function () {
-//                 console.log('Successfully updated config.xml');
-//             });
-//         });
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
+            console.log(`Updating config.xml file...`);
+            fs.writeFile(commander.pathToConfig, xml, function () {
+                console.log('Successfully updated config.xml');
+            });
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // function writePackageJson(urlName) {
 //     try {
